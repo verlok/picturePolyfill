eb1a2449212de8261187e75ad5883fa828ab8415
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2014-03-03 11:01:51 +0100

    Modular JS + timeout on resize + HTML title and intro
    Transformed the function in a module and introduced timeout after
    resize event + added title and intro paragraphs in the example html file

--- picturePolyfill.js
+++ picturePolyfill.js
@@ -1,82 +1,95 @@
-function responsiveImages() {
+(function() {
 
-	var pictureElements = document.getElementsByTagName('picture'),
-		pictureElement,
-		imgElement, srcAttribute, i,
-		pixelRatio = window.devicePixelRatio || 1;
+	var timerId;
 
-	// Parse source elements and call parseSourceElement on each one
-	function parseSourceElements(sourceElements, ignoreMedia) {
-		var matchedSrc = null, i;
-		for (i=0; i<sourceElements.length; i+=1) {
-			// Get the right srcSet based on media queries
-			matchedSrc = parseSourceElement(sourceElements[i], ignoreMedia) || matchedSrc;
+	RIM = {};
+
+	function init() {
+
+		var pictureElements = document.getElementsByTagName('picture'),
+			pictureElement,
+			imgElement, srcAttribute, i,
+			pixelRatio = window.devicePixelRatio || 1;
+
+		// Parse source elements and call parseSourceElement on each one
+		function parseSourceElements(sourceElements, ignoreMedia) {
+			var matchedSrc = null, i;
+			for (i=0; i<sourceElements.length; i+=1) {
+				// Get the right srcSet based on media queries
+				matchedSrc = parseSourceElement(sourceElements[i], ignoreMedia) || matchedSrc;
+			}
+			return matchedSrc;
 		}
-		return matchedSrc;
-	}
 
-	// Parse source element and get the proper src attribute
-	function parseSourceElement(sourceElement, ignoreMedia) {
-		var media, i, ratio, srcSetElements, srcSetElement, matchedSrc, srcSetAttribute;
-		media = sourceElement.getAttribute('media');
-		if (ignoreMedia || !media || window.matchMedia(media).matches) {
-			srcSetAttribute = sourceElement.getAttribute('srcset');
-			// Get the right source based on pixel ratio
-			srcSetElements = srcSetAttribute.split(',');
-			for (i=0; i<srcSetElements.length; i+=1) {
-				srcSetElement = srcSetElements[i].trim().split(' ');
-				ratio = parseInt(srcSetElement[1], 10) || 1;
-				if (ratio === pixelRatio) {
-					matchedSrc = srcSetElement[0];
+		// Parse source element and get the proper src attribute
+		function parseSourceElement(sourceElement, ignoreMedia) {
+			var media, i, ratio, srcSetElements, srcSetElement, matchedSrc, srcSetAttribute;
+			media = sourceElement.getAttribute('media');
+			if (ignoreMedia || !media || window.matchMedia(media).matches) {
+				srcSetAttribute = sourceElement.getAttribute('srcset');
+				// Get the right source based on pixel ratio
+				srcSetElements = srcSetAttribute.split(',');
+				for (i=0; i<srcSetElements.length; i+=1) {
+					srcSetElement = srcSetElements[i].trim().split(' ');
+					ratio = parseInt(srcSetElement[1], 10) || 1;
+					if (ratio === pixelRatio) {
+						matchedSrc = srcSetElement[0];
+					}
 				}
 			}
+			return matchedSrc;
 		}
-		return matchedSrc;
-	}
 
-	function getOrCreateImage(picture) {
-		var imageElements, imageElement;
-		imageElements = picture.getElementsByTagName('img');
+		function getOrCreateImage(picture) {
+			var imageElements, imageElement;
+			imageElements = picture.getElementsByTagName('img');
 
-		// If image already exist, return it
-		if (imageElements.length) {
-			imageElement = imageElements[0];
+			// If image already exist, return it
+			if (imageElements.length) {
+				imageElement = imageElements[0];
+			}
+			// Else create the image
+			else {
+				imageElement = document.createElement('img');
+				imageElement.setAttribute('alt', picture.getAttribute('data-alt'));
+				picture.appendChild(imageElement);
+			}
+			return imageElement;
 		}
-		// Else create the image
-		else {
-			imageElement = document.createElement('img');
-			imageElement.setAttribute('alt', picture.getAttribute('data-alt'));
-			picture.appendChild(imageElement);
-		}
-		return imageElement;
-	}
 
-	// Loop through all picture elements
-	for (i=0; i<pictureElements.length; i+=1) {
+		// Loop through all picture elements
+		for (i=0; i<pictureElements.length; i+=1) {
 
-		pictureElement = pictureElements[i];
-		
-		// If browser doesn't support matchMedia - NOTE: Paul Irish's polyfill is provided in "external/matchMedia.js"
-		srcAttribute = (window.matchMedia) ?
-			parseSourceElements(pictureElement.getElementsByTagName('source'), false) :
-			parseSourceElement(pictureElement.querySelector('source[data-default]'), true);
+			pictureElement = pictureElements[i];
 			
-		// Fallback image
-		// TODO: get from the options, to be passed in from an init function
-		srcAttribute = srcAttribute || "http://placehold.it/1x1";
+			// If browser doesn't support matchMedia - NOTE: Paul Irish's polyfill is provided in "external/matchMedia.js"
+			srcAttribute = (window.matchMedia) ?
+				parseSourceElements(pictureElement.getElementsByTagName('source'), false) :
+				parseSourceElement(pictureElement.querySelector('source[data-default]'), true);
+				
+			// Fallback image
+			// TODO: get from the options, to be passed in from an init function
+			srcAttribute = srcAttribute || "http://placehold.it/1x1";
 
-		// Select the image, or create it
-		imgElement = getOrCreateImage(pictureElement);
+			// Select the image, or create it
+			imgElement = getOrCreateImage(pictureElement);
 
-		// Set the img source
-		imgElement.setAttribute('src', srcAttribute);
+			// Set the img source
+			imgElement.setAttribute('src', srcAttribute);
+		}
 	}
-}
 
-// On resize
-window.addEventListener('resize', function() {
-	responsiveImages();
-});
+	// On resize
+	window.addEventListener('resize', function() {
+		clearTimeout(timerId);
+		timerId = setTimeout(function() {
+			init();
+		}, 500);
+	});
+
+	RIM.responsiveImages = init;
+
+}());
 
 // Execute the function right at page landing
-responsiveImages();
+RIM.responsiveImages();
