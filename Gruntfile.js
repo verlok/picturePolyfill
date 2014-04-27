17382781c57670413da10bb68d109b944effdb7a
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2014-04-27 17:43:48 +0200

    Version bump to 3.3.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		// Metadata.
 		meta: {
-			version: '3.3.0'
+			version: '3.3.1'
 		},
 		banner: '/*! picturePolyfill - v<%= meta.version %> - ' +
 			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
