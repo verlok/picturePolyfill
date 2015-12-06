6460b57c15d254ab6d0bb1a88eaddf1442cf9c02
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2015-12-06 09:52:11 +0100

    Updated version

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		// Metadata.
 		meta: {
-			version: '4.2.0'
+			version: '4.2.1'
 		},
 		banner: '/*! picturePolyfill - v<%= meta.version %> - ' +
 			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
